0f4a816ccb9172c2125986b88c3efb2a4e916a76
enidev911 <enidev911@gmail.com>
2022-10-16 05:51:55 -0300

    up

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-// para poder usar ESM debemos tener una versión de NODE superior a la 16
-import express from 'express';
-import routesEmployees from './routes/employees.routes.js';
-import routesIndex from './routes/index.routes.js';
+import app from './app.js'
 import { PORT } from './config.js'
-
-const app = express();
-// middlewares before
-app.use(express.json());
-
-// rutas
-app.use('/api', routesEmployees);
-app.use(routesIndex);
-
-// middleware after
-app.use((req, res, next) => {
-    res.status(404).json({
-        message: "endpoint no encontrado"
-    })
-})
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
